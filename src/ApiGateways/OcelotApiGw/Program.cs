96ae222bd6301201cb6e7ae305c20b29e7033d48
Wageh Ayed <wageh.ayed@gmail.com>
2023-07-17 01:05:00 +0300

    apply Dockerfile for all Projects

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -5,9 +5,10 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Common.Logging;
+using System.Threading.Tasks; 
 using Serilog;
+using Common.Logging;
+
 namespace OcelotApiGw
 {
     public class Program
